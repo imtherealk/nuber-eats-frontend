e18f67c84f60f32a353300b6c79c1f848afa0f43
Nayeong <imtherealk@gmail.com>
2021-01-03 08:46:49 +0900

    Add login/create-account routes to LoggedOutRouter

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,64 +1,19 @@
 import React from "react";
-import { useForm } from "react-hook-form";
-import { isLoggedInVar } from "../apollo";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { CreateAccount } from "../pages/create-account";
+import { Login } from "../pages/login";
 
-interface IForm {
-  email: string;
-  password: string;
-}
 export const LoggedOutRouter = () => {
-  const { register, watch, handleSubmit, errors } = useForm<IForm>();
-  const onSubmit = () => {
-    console.log(watch());
-  };
-
-  const onInvalid = () => {
-    console.log(errors);
-    console.log("cannot create account");
-  };
   return (
-    <div>
-      <h1>Logged Out</h1>
-
-      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
-        <div>
-          <input
-            ref={register({
-              required: "This is required",
-              validate: {
-                gmail: (email: string) => email.includes("@gmail.com"),
-              },
-            })}
-            name="email"
-            type="email"
-            placeholder="email"
-          />
-          {errors.email?.message && (
-            <span className="font-bold text-red-600">
-              {errors.email?.message}
-            </span>
-          )}
-          {errors.email?.type === "gmail" && (
-            <span className="font-bold text-red-600">Use gmail</span>
-          )}
-        </div>
-        <div>
-          <input
-            ref={register({
-              required: "This is required",
-            })}
-            name="password"
-            type="password"
-            placeholder="password"
-          />
-          {errors.password?.message && (
-            <span className="font-bold text-red-600">
-              {errors.password?.message}
-            </span>
-          )}
-        </div>
-        <button className="bg-yellow-300 text-white">submit</button>
-      </form>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/create-account">
+          <CreateAccount />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
