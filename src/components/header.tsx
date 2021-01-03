678ee014fa688cb12ac05164f20c5628e67ed3f9
Nayeong <imtherealk@gmail.com>
2021-01-04 07:48:03 +0900

    Modify header container max width

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ export const Header: React.FC = () => {
   const { data } = useMe();
   return (
     <header className="py-5">
-      <div className="w-full px-5 xl:px-0 max-w-7xl mx-auto flex justify-between items-center">
+      <div className="w-full px-5 xl:px-0 max-w-screen-2xl mx-auto flex justify-between items-center">
         <img src={nuberLogo} alt="nuber logo" className="w-36" />
         <span className="text-xs">
           <Link to="/my-profile">
