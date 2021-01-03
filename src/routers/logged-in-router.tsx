ae9f6ca02d9fe3fe18a33f923bdde7464d7d4f1b
Nayeong <imtherealk@gmail.com>
2021-01-04 04:53:41 +0900

    Set headers with auth-token

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,16 +1,39 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import { meQuery } from "../api-types/meQuery";
 import { authTokenVar, isLoggedInVar } from "../apollo";
 import { LOCALSTORAGE_TOKEN } from "../constants";
 
+const ME_QUERY = gql`
+  query meQuery {
+    me {
+      id
+      email
+      role
+      verified
+    }
+  }
+`;
+
 export const LoggedInRouter = () => {
+  const { data: data, loading, error } = useQuery<meQuery>(ME_QUERY);
+
   const logout = () => {
     localStorage.removeItem(LOCALSTORAGE_TOKEN);
     authTokenVar(null);
     isLoggedInVar(false);
   };
+
+  if (!data || loading || error) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <span className="font-medium text-lg tracking-wide">Loading..</span>
+      </div>
+    );
+  }
   return (
     <div>
-      <h1>Logged In</h1>
+      <h1>{data.me.email}</h1>
       <button onClick={logout}>click to logout</button>
     </div>
   );
