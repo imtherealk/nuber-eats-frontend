f7c176e5f23936620afccfe74dfc0e58caed7d92
Nayeong <imtherealk@gmail.com>
2021-01-04 14:36:24 +0900

    Add home link to logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,8 +9,10 @@ export const Header: React.FC = () => {
   const { data } = useMe();
   return (
     <header className="py-5">
-      <div className="w-full px-5 xl:px-0 max-w-screen-2xl mx-auto flex justify-between items-center">
-        <img src={nuberLogo} alt="nuber logo" className="w-36" />
+      <div className="w-full  px-5 xl:px-1 max-w-7xl mx-auto flex justify-between items-center">
+        <Link to="/">
+          <img src={nuberLogo} alt="nuber logo" className="w-36" />
+        </Link>
         <span className="text-xs">
           <Link to="/my-profile">
             <FontAwesomeIcon icon={faUserAlt} className="text-lg" />
